d2aef7dd3141907e39398f0589954f9eafb46cc9
rahulKasana <18752429+rahulkasana@users.noreply.github.com>
2021-07-13 14:16:46 +0530

    light bg color

--- src/darkToggle.js
+++ src/darkToggle.js
@@ -36,7 +36,7 @@ export default function DarkToggle() {
           xmlns="http://www.w3.org/2000/svg"
           className="h-8 w-8 mr-2"
           viewBox="0 0 20 20"
-          fill={isDark ? "gray" : "yellow"}
+          fill={isDark ? "gray" : "orange"}
         >
           <path
             fillRule="evenodd"
