d84bbc40b567955549105fa52d668f6913f6d80f
rahulKasana <18752429+rahulkasana@users.noreply.github.com>
2021-07-13 10:19:51 +0530

    light mode done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Switch from "react-switch";
 import "./App.css";
 
 function App() {
+  const [isDark, setDark] = useState(false);
   useEffect(() => {
     if (
       localStorage.theme === "dark" ||
@@ -9,8 +11,10 @@ function App() {
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.querySelector("html").classList.add("dark");
+      setDark(true);
     } else if (localStorage.theme === "dark") {
       document.querySelector("html").classList.add("dark");
+      setDark(true);
     }
   }, []);
 
@@ -19,41 +23,26 @@ function App() {
     if (localStorage.theme == "dark") {
       htmlClasses.remove("dark");
       localStorage.removeItem("theme");
+      setDark(false);
     } else {
       htmlClasses.add("dark");
       localStorage.theme = "dark";
+      setDark(true);
     }
   };
 
   const item =
-    "flex justify-center items-center select-none shadow bg-white rounded-3xl";
+    "flex justify-center items-center select-none rounded-3xl digit dark:digit-dark";
   return (
-    <div className="flex h-full w-full flex-col bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 dark:bg-black">
-      <button
-        onClick={toggleDark}
-        id="switchTheme"
-        className="h-10 w-10 flex justify-center items-center focus:outline-none text-yellow-500"
-      >
-        <svg
-          className="w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </button>
+    <div className="flex h-full w-full flex-col bg-light dark:bg-dark">
+      <Switch onChange={toggleDark} checked={isDark} />
       <div className="flex h-full w-full flex-col justify-center items-center">
-        <div className="flex flex-col h-4/5 w-full sm:w-4/5 md:w-3/5 lg:w-5/12 bg-blue-200 rounded-3xl overflow-hidden">
-          <div className="flex flex-col h-1/4 shadow-inner bg-white rounded-3xl px-4 m-4">
-            <div className="flex flex-row-reverse h-1/2 items-center flex-nowrap overflow-x-auto text-right text-base">
+        <div className="flex flex-col h-4/5 w-full sm:w-4/5 md:w-3/5 lg:w-5/12 shadow rounded-3xl overflow-hidden calculator-container dark:calculator-container-dark">
+          <div className="flex flex-col h-1/4 result-container dark:result-container-dark rounded-3xl px-4 m-4">
+            <div className="flex flex-row-reverse h-1/2 items-center flex-nowrap overflow-x-auto text-right text-xl">
               23876872578357265376257536*32657235763257623576357253478634876834687346874372563
             </div>
-            <div className="flex flex-row-reverse items-center text-xl">
+            <div className="flex flex-row-reverse h-1/2 items-center text-4xl">
               result
             </div>
           </div>
